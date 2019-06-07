e3c521e786f02bd1633b79fca13f8e2edab61e1c
Pozetiv <pozetivteam@gmail.com>
2019-06-07 18:11:37 +0300

    update for local projects

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,11 @@
-<?php
-
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
+       
     </head>
     <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
+    	<?php phpinfo() ?>
+        <h1>Docker ?></h1>
     </body>
 </html>
